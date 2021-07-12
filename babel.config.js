12684ec03127856331ea167b0ae196187f3c8d25
Kemal Ahmed <goatandsheep@gmail.com>
2021-07-12 12:47:22 -0400

    added dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,6 @@
 module.exports = function(api) {
     api.cache(false);
-    const presets = [['@babel/preset-env',
-    {
-        "targets": {
-            "chrome": "58",
-            "ie": "11"
-        }
-    }], '@babel/preset-react'];
+    const presets = ['module:metro-react-native-babel-preset']
     const plugins = [
         ['module:react-native-dotenv', {
             'moduleName': 'react-native-dotenv'
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function HelloWorld() {
+    return (
+        <h1>Javascript works</h1>
+    );
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+
 console.log('Javascript works');
